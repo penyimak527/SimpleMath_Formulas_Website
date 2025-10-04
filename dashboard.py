cf832c78fba214e8e66d2db2ec949acc0ce270ee
penyimak527 <143236724+penyimak527@users.noreply.github.com>
2025-10-04 07:26:59 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,8 +3,9 @@ import streamlit as st
 st.write("""
         # Selamat Datang di Dashboard Website Ini
         Ini adalah halaman utama untuk menuju ke website lain
-        Ini duibuat dengan Python & Streamlit.""")
+        Ini dibuat dengan Python & Streamlit.""")
 
-st.info("👈 Gunakan menu di sidebar atau tombol di damping untuk menjelajahi fitur.") # info sementara
+st.info("👈 Gunakan menu di sidebar atau tombol di samping untuk menjelajahi fitur.") # info sementara
 st.caption("Dibuat tahun 2025")
 st.balloons()
+
